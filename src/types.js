14c10acad02db5a9dc7b0a5cb9fd2be706546efe
bruce qin <bruceqin@bruces-MacBook-Pro.local>
2022-04-05 21:47:27 -0700

    init

---
+++ src/types.js
@@ -0,0 +1,12 @@
+/*
+  These are the different action types the game duck can use.
+*/
+
+const NEW_GAME = "GAME_NEW";
+const GAMEOVER = "GAME_OVER";
+const MOVE = "GAME_MOVE";
+const PLAYER = "GAME_PLAYER";
+const WINNER = "GAME_WINNER";
+const WINNERCOUNT = "GAME_WINNER_COUNT";
+
+export { NEW_GAME, GAMEOVER, MOVE, PLAYER, WINNER, WINNERCOUNT };
